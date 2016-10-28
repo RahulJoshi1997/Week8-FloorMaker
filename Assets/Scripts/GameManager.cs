4a888076b53e831e91e082369760ec261db4037b
Rahul Joshi <rahuljoshi@172-16-119-136.DYNAPOOL.NYU.EDU>
2016-10-27 23:02:32 -0400

    The thing's done. The Floor generates choosing different tiles. And the camera moves and zooms to accommodate the entire map as it changes.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	//Count the number of floor tiles and pathmakers
+	public int floorCount = 0;
+	public int pathMakerCount = 0;
+
+	public float zMax = 0f;
+	public float zMin = 0f;
+	public float xMax = 0f;
+	public float xMin = 0f;
+
+	public GameObject cam;
+	Vector3 cameraPos = new Vector3(0f,0f,0f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Moving Camera
+		cameraPos.x = (xMin + xMax) / 2f;
+		cameraPos.z = (zMin + zMax) / 2f;
+
+		float zoomFactor = 0;
+		if (Mathf.Abs(xMin) + Mathf.Abs(xMax) > Mathf.Abs(zMin) + Mathf.Abs(zMax))
+			zoomFactor = Mathf.Abs(xMin) + Mathf.Abs(xMax);
+		if (Mathf.Abs(xMin) + Mathf.Abs(xMax) < Mathf.Abs(zMin) + Mathf.Abs(zMax))
+			zoomFactor = Mathf.Abs(zMin) + Mathf.Abs(zMax);
+		cameraPos.y = zoomFactor;
+
+		cam.transform.position = cameraPos;
+
+		//Have a way to regenerate the map in WebGL
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene ("Floors");
+		}
+
+	}
+}
---
+++ Assets/Scripts/PathMaker.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathMaker : MonoBehaviour {
+
+	GameManager gm;
+
+	int counter = 0;
+	float turnProbablity;
+	float babyProbablity;
+	int counterLimit;
+
+	public Transform floorPrefab1;
+	public Transform floorPrefab2;
+	public Transform floorPrefab3;
+	public Transform pathmakerSpherePrefab;
+
+	// Use this for initialization
+	void Start () {
+		gm = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+
+		//Randomize some elements of the pathMaker
+		turnProbablity = Random.Range (0.1f, 0.20f);
+		babyProbablity = Random.Range (0.75f, 0.99f);
+		counterLimit = Random.Range (10, 50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Each pathMaker produces only a certain amount of floor tiles
+		if (counter < counterLimit) {
+			float rand = Random.value;
+			if (rand < turnProbablity) {
+				this.transform.Rotate (new Vector3 (0f, 90f, 0f));
+			} else if (rand < turnProbablity * 2f) {
+				this.transform.Rotate (new Vector3 (0f, -90f, 0f));
+			} else if (rand > babyProbablity && gm.pathMakerCount < 5) {
+				Instantiate (pathmakerSpherePrefab, this.transform.position, this.transform.rotation);
+				gm.pathMakerCount++;
+			}
+
+			int floorChoice = Random.Range (1, 4);
+			if (floorChoice == 1) {
+				Instantiate (floorPrefab1, this.transform.position, Quaternion.Euler (0f, 0f, 0f));
+			} else if (floorChoice == 2) {
+				Instantiate (floorPrefab2, this.transform.position, Quaternion.Euler (0f, 0f, 0f));
+			} else if (floorChoice == 3) {
+				Instantiate (floorPrefab3, this.transform.position, Quaternion.Euler (0f, 0f, 0f));
+			}
+			gm.floorCount++;
+
+			transform.position += transform.forward * 5f;
+			if (this.transform.position.x > gm.xMax)
+				gm.xMax = this.transform.position.x;
+			if (this.transform.position.x < gm.xMin)
+				gm.xMin = this.transform.position.x;
+			if (this.transform.position.z > gm.zMax)
+				gm.zMax = this.transform.position.z;
+			if (this.transform.position.z < gm.zMin)
+				gm.zMin = this.transform.position.z;
+
+			counter++;
+		} else {	//Get rid of the pathMaker after the counterLimit has been reached.
+			Destroy (this.gameObject);
+			gm.pathMakerCount--;
+		}
+
+		//Limit the maximum number of floor tiles.
+		if (gm.floorCount >= 1000) {
+			Destroy (this.gameObject);
+		}
+	}
+}
